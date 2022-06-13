fd65cc65d8abdf327f932f4e92434f0400a4ad2c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-13 05:06:56 -0400

    Add files via upload

---
+++ py_folder_current_001.py
@@ -0,0 +1,15 @@
+# Dedicated to God the Father
+# All Rights Reserved Christopher Topalian Copyright 2000-2022
+# py_folder_current_001.py
+# getcwd gets the path of the current working directory (cwd)
+# cwd means current working directory
+# another word for directory is folder
+# shows the path of the folder where this script is currently working from
+
+import os
+
+theFolderPath = os.getcwd()
+
+print(theFolderPath)
+
+input("Press Enter to Exit")
