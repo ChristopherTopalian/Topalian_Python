b4031ba10e865390d8fcef4a795a14ea551f2a29
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-27 11:02:34 -0400

    Add files via upload

---
+++ py_name_of_user_001.py
@@ -0,0 +1,10 @@
+# Dedicated to God the Father
+# All Rights Reserved Christopher Topalian Copyright 2000-2022
+# py_name_of_user_001.py
+# shows the name of the user
+
+from getpass import getuser
+
+print(getuser())
+
+input("Press Enter to Exit")
