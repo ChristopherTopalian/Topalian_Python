b81203aeca67da49dc0e90ef47241bf071481457
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-27 11:55:42 -0400

    Update py_name_of_user_001.py

--- py_name_of_user_001.py
+++ py_name_of_user_001.py
@@ -1,5 +1,6 @@
 # Dedicated to God the Father
 # All Rights Reserved Christopher Topalian Copyright 2000-2022
+# https://github.com/ChristopherTopalian
 # py_name_of_user_001.py
 # shows the Username using print
 
