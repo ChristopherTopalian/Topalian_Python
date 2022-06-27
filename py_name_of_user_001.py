bd06e3028378fc0cef7b14393f8657dc1f53cbfb
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-27 11:46:32 -0400

    Update py_name_of_user_001.py

--- py_name_of_user_001.py
+++ py_name_of_user_001.py
@@ -1,7 +1,7 @@
 # Dedicated to God the Father
 # All Rights Reserved Christopher Topalian Copyright 2000-2022
 # py_name_of_user_001.py
-# shows the name of the user
+# shows the Username using print
 
 from getpass import getuser
 
