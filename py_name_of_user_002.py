a37394f20560c6b04df36951b2dda57b6f56c96a
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-27 11:37:32 -0400

    Add files via upload

---
+++ py_name_of_user_002.py
@@ -0,0 +1,25 @@
+# Dedicated to God the Father
+# All Rights Reserved Christopher Topalian Copyright 2000-2022
+# https://github.com/ChristopherTopalian
+# py_name_of_user_002.py
+# shows the name of the user using print and MessageBox
+
+import ctypes
+from getpass import getuser
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+theTitle = "Username"
+
+theUsername = getuser()
+
+theText = "Username is:"
+
+theMessage = theText + "\n\n" + theUsername  
+
+print(theMessage)
+
+ctypes.windll.user32.MessageBoxW(0,
+theMessage, theTitle, 0)
+
+input("Press Enter to Exit")
