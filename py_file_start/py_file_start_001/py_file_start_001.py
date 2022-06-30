8d8130c0cc4db423ba8c7609396d64be40950142
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-30 11:43:53 -0400

    Add files via upload

---
+++ py_file_start/py_file_start_001/py_file_start_001.py
@@ -0,0 +1,12 @@
+# Dedicated to God the Father
+# All Rights Reserved Christopher Topalian Copyright 2000-2022
+# https://github.com/ChristopherTopalian
+# py_file_start_001.py
+# opens the file that we specify by name and extension,
+# the file will open in the default application of the extension type specified
+
+import os
+
+fileName = "page_001.html"
+
+os.startfile(fileName)
