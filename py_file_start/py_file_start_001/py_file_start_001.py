aa384e00da6282500484b4cd6d37ce05bd1f2bc6
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-30 11:44:39 -0400

    Update py_file_start_001.py

--- py_file_start/py_file_start_001/py_file_start_001.py
+++ py_file_start/py_file_start_001/py_file_start_001.py
@@ -2,7 +2,7 @@
 # All Rights Reserved Christopher Topalian Copyright 2000-2022
 # https://github.com/ChristopherTopalian
 # py_file_start_001.py
-# opens the file that we specify by name and extension,
+# opens the file that we specify by name and extension
 # the file will open in the default application of the extension type specified
 
 import os
